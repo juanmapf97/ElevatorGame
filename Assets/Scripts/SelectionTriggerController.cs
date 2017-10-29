cbeed0e490fa2d67b951291c69c6fd5c69d03ba3
juanmapf97 <juanmapf97@gmail.com>
2017-10-29 00:00:06 -0500

    Added elevator movement on click

---
+++ Assets/Scripts/SelectionTriggerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionTriggerController : MonoBehaviour {
+
+    public ElevatorController elevator;
+    public int floor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown ()
+    {
+        elevator.MoveToFloor(floor);
+    }
+}
